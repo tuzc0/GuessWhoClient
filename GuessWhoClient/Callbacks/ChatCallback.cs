f1c2cee1ae79ec750a6af27bc637c7311f37d7c8
AlejandroMartinez5 <martinezramirezalejandro02@gmail.com>
2025-10-19 21:31:24 -0600

    Add WPF ChatWindow to test ChatService with duplex callback

---
+++ GuessWhoClient/Callbacks/ChatCallback.cs
@@ -0,0 +1,19 @@
+﻿using GuessWhoClient.ChatServiceRef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GuessWhoClient.Callbacks
+{
+
+    public class ChatCallback : IChatServiceCallback
+    {
+        public void ReceiveMessage(string user, string message)
+        {
+            Console.WriteLine($"{user}: {message}");
+        }
+    }
+}
---
+++ GuessWhoClient/ChatWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using GuessWhoClient.ChatServiceRef;
+using GuessWhoClient.Callbacks;
+using System;
+using System.ServiceModel;
+using System.Windows;
+
+namespace GuessWhoClient
+{
+    public partial class ChatWindow : Window
+    {
+        private IChatService chatProxy;
+
+        public ChatWindow()
+        {
+            InitializeComponent();
+
+            // Crear el callback y el canal duplex
+            var callback = new ChatCallbackWrapper(this);
+            var context = new InstanceContext(callback);
+            var factory = new DuplexChannelFactory<IChatService>(context, "NetTcpBinding_IChatService");
+            chatProxy = factory.CreateChannel();
+
+            // Unirse al chat
+            chatProxy.Join("UsuarioPrueba");
+        }
+
+        private void BtnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtMessage.Text))
+            {
+                chatProxy.SendMessage("UsuarioPrueba", txtMessage.Text);
+                txtMessage.Clear();
+            }
+        }
+
+        // Método para agregar mensajes a la TextBox desde el callback
+        public void AddMessage(string user, string message)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                txtChat.AppendText($"{user}: {message}\n");
+                txtChat.ScrollToEnd();
+            });
+        }
+    }
+
+    // Wrapper de callback para actualizar la UI
+    public class ChatCallbackWrapper : IChatServiceCallback
+    {
+        private readonly ChatWindow window;
+
+        public ChatCallbackWrapper(ChatWindow window)
+        {
+            this.window = window;
+        }
+
+        public void ReceiveMessage(string user, string message)
+        {
+            window.AddMessage(user, message);
+        }
+    }
+}
